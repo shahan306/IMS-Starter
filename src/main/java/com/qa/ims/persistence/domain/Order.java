f284b88b7343102ce77fa65d59830b8de4d5de1e
shahan306 <shahan306@gmail.com>
2022-07-27 21:25:13 +0100

    added orderDAO

--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -1,5 +1,73 @@
 package com.qa.ims.persistence.domain;
 
-public class Order {
+import java.util.Objects;
 
+public class Order {
+	
+	private Long id;
+	private String customer_id;
+	
+	
+	// constructor with id
+	public Order(Long id, String customer_id) {
+		super();
+		this.id = id;
+		this.customer_id = customer_id;
+	}
+
+	//constructor without id
+	public Order(String customer_id) {
+		super();
+		this.customer_id = customer_id;
+	}
+	// getters and setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCustomer_id() {
+		return customer_id;
+	}
+
+	public void setCustomer_id(String customer_id) {
+		this.customer_id = customer_id;
+	}
+
+	
+	
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", customer_id=" + customer_id + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(customer_id, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(customer_id, other.customer_id) && Objects.equals(id, other.id);
+	}
+	
+	
+
+	
+		
+		
+		
+		
+	
+	
 }
